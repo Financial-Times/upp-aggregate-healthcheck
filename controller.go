5d7d3d7ab184411bc4c88cdcb10533444e2e5146
Ioana Ciont <ioana.ciont@ft.com>
2018-10-16 17:00:20 +0300

    Fixing Kafka blips (2nd attempt)

--- controller.go
+++ controller.go
@@ -193,6 +193,7 @@ func (c *healthCheckController) disableStickyFailingCategories(categories map[st
 			for _, healthCheck := range healthChecks {
 				if healthCheck.Name == serviceName && !healthCheck.Ok {
 					c.stickyCategoriesFailedServices[serviceName]++
+					infoLogger.Printf("Sticky category [%s] is unhealthy -- check %v/%v.", category.name, c.stickyCategoriesFailedServices[serviceName], category.failureThreshold)
 
 					if c.isCategoryThresholdExceeded(serviceName, category.failureThreshold) {
 						infoLogger.Printf("Sticky category [%s] is unhealthy, disabling it.", category.name)
@@ -204,11 +205,7 @@ func (c *healthCheckController) disableStickyFailingCategories(categories map[st
 							errorLogger.Printf("Cannot disable sticky category with name %s. Error was: %s", category.name, err.Error())
 						}
 						c.stickyCategoriesFailedServices[serviceName] = 0
-					} else {
-						infoLogger.Printf("Sticky category [%s] is unhealthy -- check %v/%v.", category.name, c.stickyCategoriesFailedServices[serviceName], category.failureThreshold)
 					}
-				} else if healthCheck.Ok {
-					c.stickyCategoriesFailedServices[serviceName] = 0
 				}
 			}
 		}
